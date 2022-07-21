19ac50e9265f88653542f7de59a963d73939ced1
Tai Zeng <zengtai@gmail.com>
2022-07-22 02:02:14 +0800

    略微细化

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { SITE_META } from "../lib/constants";
 
 export default function Footer(params) {
   return (
@@ -8,7 +9,10 @@ export default function Footer(params) {
           <Link href={`/t/privacy-policy`}>Privacy Policy</Link>
           <Link href={`/t/terms-of-use`}>Terms of Use</Link>
         </nav>
-        <p>Copyright &copy; {new Date().getFullYear()}</p>
+        <p>
+          Copyright &copy; {new Date().getFullYear()} {SITE_META.name}. All
+          Rights Reserved.
+        </p>
       </footer>
     </>
   );
