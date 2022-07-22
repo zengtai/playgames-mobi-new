6304938b0eb02255341ff15269587b17b40fe58e
Tai Zeng <zengtai@gmail.com>
2022-07-22 09:33:08 +0800

    暂存

--- components/Banner.js
+++ components/Banner.js
@@ -1,7 +1,39 @@
-export default function Banner(params) {
+import { useEffect } from "react";
+import { ADS_ID } from "../lib/constants";
+
+export default function Banner({
+  className,
+  style,
+  layout,
+  format,
+  client = ADS_ID,
+  slot,
+  responsive,
+  layoutKey,
+  auto,
+}) {
+  useEffect(() => {
+    try {
+      const adsbygoogle = window.adsbygoogle || [];
+      adsbygoogle.push({});
+    } catch (e) {
+      console.error(`Adsense Error: `, e.messenge);
+    }
+  });
   return (
     <>
-      <ins className="adsbygoogle"></ins>
+      <div className={className}>
+        <ins
+          className="adsbygoogle"
+          style={style}
+          data-ad-layout={layout}
+          data-ad-format={format}
+          data-ad-client={client}
+          data-ad-slot={slot}
+          data-ad-layout-key={layoutKey}
+          data-full-width-responsive={responsive}
+        />
+      </div>
     </>
   );
 }
